bcb8ef0cf35a1b653c714f2e020c27dd8a6d3f3c
Christoph Knauf <christoph.knauf@itemis.de>
2016-07-17 14:30:29 +0200

    Navigation

--- index_files/foot.js
+++
@@ -1,9 +0,0 @@
-document.write('\
-\
-<div class="mastfood">\
-  <div class="inner">\
-    <p><a href="http:/ /getbootstrap.com/">Impressum</a></p>\
-  </div>\
-</div>\
-\
-');
--- index_files/nav.js
+++ index_files/nav.js
@@ -1,17 +1,14 @@
-document.write('\
-\
-<div class="masthead clearfix">\
-  <div class="inner">\
-    <h3 class="masthead-brand">Manufaktion</h3>\
-       <nav>\
-          <ul class="nav masthead-nav">\
-            <li><a href="index.html">Home</a></li>\
-            <li><a href="bikes.html">Bikes</a></li>\
-            <li><a href="contact.html">Kontakt</a></li>\
-            <li><a href="links.html">Links</a></li>\
-          </ul>\
-       </nav>\
-  </div>\
-</div>\
-\
-');
+$(document).ready(function() {
+        $(".content").load("home.html");
+});
+
+$("ul.nav a").each(function() {
+    $(this).on("click", function(){
+        $(".content").load($(this).attr("data-page"));
+    });
+});
+
+$(".nav a").on("click", function(){
+   $(".nav").find(".active").removeClass("active");
+   $(this).parent().addClass("active");
+});
