6d0a6505a887abe7c923f196f5d65754d258e32f
Christoph Knauf <christoph.knauf@itemis.de>
2016-07-17 12:03:48 +0200

    Linking

---
+++ index_files/foot.js
@@ -0,0 +1,9 @@
+document.write('\
+\
+<div class="mastfoot">\
+  <div>\
+    <p><a href="http://getbootstrap.com/">Impressum</a></p>\
+  </div>\
+</div>\
+\
+');
---
+++ index_files/nav.js
@@ -0,0 +1,17 @@
+document.write('\
+\
+<div class="masthead clearfix">\
+  <div class="inner">\
+    <h3 class="masthead-brand">Manufaktion</h3>\
+       <nav>\
+          <ul class="nav masthead-nav">\
+            <li><a href="index.html">Home</a></li>\
+            <li><a href="bikes.html">Bikes</a></li>\
+            <li><a href="contact.html">Kontakt</a></li>\
+            <li><a href="links.html">Links</a></li>\
+          </ul>\
+       </nav>\
+  </div>\
+</div>\
+\
+');
