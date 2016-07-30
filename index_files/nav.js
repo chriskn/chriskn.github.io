cae2e9a9d0c3c4a16c4d3af8a6398469fc131dc3
Christoph Knauf <christoph.knauf@itemis.de>
2016-07-30 14:49:12 +0200

    bikes

--- index_files/nav.js
+++ index_files/nav.js
@@ -8,6 +8,12 @@ $("ul.nav a").each(function() {
     });
 });
 
+$("div a").each(function() {
+    $(this).on("click", function(){
+        $(".content").load($(this).attr("bikes/data-page"));
+    });
+});
+
 $(".nav a").on("click", function(){
    $(".nav").find(".active").removeClass("active");
    $(this).parent().addClass("active");
